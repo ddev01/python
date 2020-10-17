135518ab71c4a45acee3cb35443eca17eae65930
Admin <douweduyster@gmail.com>
2020-10-17 20:17:11 +0200

    test

--- H20/1.py
+++ H20/1.py
@@ -1,29 +1,27 @@
-from copy import copy
 class Point:
-    def __init__( self , x=0.0 , y=0.0 ):
+    def __init__( self, x=0.0, y=0.0 ):
         self.x = x
         self.y = y
     def __repr__( self ):
         return "({}, {})".format( self.x, self.y )
 
-class Rechthoek:
-    def __init__( self , point , width , height ):
-        self.point = copy( point )
+class Rectangle:
+    def __init__( self, point, width, height ):
+        self.point = point
         self.width = width
         self.height = height
     def __repr__( self ):
-        return "[{},b={},h={}]".format( self.point , self.width , self.height )
-    def getbotright(self):
-        return Point(self.point.x + self.width, self.point.y)
-    def getoppervlak(self):
-        return
+        if self.width >= 0 and self.height >= 0:
+                return "[{},w={},h={}]".format( self.point, self.width, self.height )
+        else:
+                return "Error, negative values"
 
-p = Point( 0.0, 0.0 )
-r = Rechthoek( p, 6.0, 6.0 )
-print(r.getbotright())
-
-
-p2 = Point( 3.0, 3.0 )
-r2 = Rechthoek( p2, 6.0, 6.0 )
+p = Point( 3.5, 5.0 )
+print(p)
+print(type(p))
+r = Rectangle( p, -4.0, 2.0 )
 print( r )
-print(r2)
+
+p.x = 1.0
+p.y = 1.0
+print( r )
